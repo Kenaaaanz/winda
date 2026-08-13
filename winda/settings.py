8cdd7ad8e53b8e5b47d93811710f0eba52cc9e06
kenaaaanz <gichabakenani@gmail.com>
2026-08-13 08:54:06 +0300

    COMMIT9: ALLAUTH AND LOGIN4

--- winda/settings.py
+++ winda/settings.py
@@ -179,11 +179,11 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
 EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
-EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='gichabakenani@gmail.com')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='windaafricasupport@gmail.com')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='kpdi btxe ouuh imae')
 
 # Default sender email
-DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='gichabakenani@gmail.com')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='windaafricasupport@gmail.com')
 
 # Admin email for order notifications
 ADMIN_EMAIL = [
