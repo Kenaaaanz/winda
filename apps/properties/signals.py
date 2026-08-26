478fa5c20913680ec6edd52aed15ac42b7d43f6e
kenaaaanz <gichabakenani@gmail.com>
2026-08-26 20:30:18 +0300

    COMMIT23: PROPERTY SIGNALS

--- apps/properties/signals.py
+++ apps/properties/signals.py
@@ -1,6 +1,7 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from .models import Property, PropertyImage
+from django.db.models.signals import pre_delete
 
 @receiver(post_save, sender=Property)
 def update_owner_stats_on_save(sender, instance, created, **kwargs):
@@ -30,3 +31,13 @@ def update_main_image(sender, instance, created, **kwargs):
     if created and instance.is_main:
         # Unset other main images
         instance.property.property_images.filter(is_main=True).exclude(id=instance.id).update(is_main=False)
+
+@receiver(pre_delete, sender=Property)
+def clear_analytics_events_on_property_delete(sender, instance, **kwargs):
+    """Clear analytics events reference before deleting property"""
+    try:
+        # Set all related analytics events to have null property
+        instance.analytics_events.all().update(property=None)
+    except Exception as e:
+        # Log error but don't prevent deletion
+        print(f"Error clearing analytics events: {e}")
